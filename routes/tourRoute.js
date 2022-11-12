9cf58e08d615d1ff499bda9e5d6c80d51be06291
Golam Mursalin <golammursalin309@gmail.com>
2022-11-12 22:05:50 +0600

    Initialize project

---
+++ routes/tourRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {
+  getAllTours,
+  createATour,
+  getATour,
+  updateATour,
+  deleteATour,
+  checkId,
+} = require('./../controllers/tourController');
+
+const router = express.Router();
+
+// router.param('id', checkId);
+router.route('/').get(getAllTours).post(createATour);
+router.route('/:id').get(getATour).delete(deleteATour).patch(updateATour);
+
+module.exports = router;
